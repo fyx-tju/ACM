db210cce8ef8e45171001e8a9cdf7b245a772513
fyx-tju <fuyuxin_tju@163.com>
2018-01-24 22:51:24 +0800

    add project

---
+++ acm/build/CMakeFiles/acm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "acm.moc"
+  "CMakeFiles/acm.dir/acm.cpp.o"
+  "CMakeFiles/acm.dir/main.cpp.o"
+  "acm.pdb"
+  "acm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/acm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
